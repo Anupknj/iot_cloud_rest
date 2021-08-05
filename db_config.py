2b5d22cd984bb0debeefc06f330f7bb249dc3831
Anup joshi <39598803+Anupknj@users.noreply.github.com>
2021-08-05 01:53:19 -0600

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -5,7 +5,7 @@ mysql = MySQL()
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '5p&0y@TRqo4L'
-app.config['MYSQL_DATABASE_DB'] = 'ECE531'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Anup@0625'
+app.config['MYSQL_DATABASE_DB'] = 'students'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
